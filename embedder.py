7dd678394f337d1c93cb2df2b6307897fbb313c4
Aarav <gamingtiger2.0.12072010@gmail.com>
2026-05-12 17:21:51 +0530

    Replace InferenceClient with SentenceTransformer

--- embedder.py
+++ embedder.py
@@ -1,16 +1,15 @@
 import os
 import numpy as np
-from huggingface_hub import InferenceClient
+from sentence_transformers import SentenceTransformer
 
 class Embedder:
     def __init__(self):
-        self.client = InferenceClient(token=os.environ["HF_TOKEN"])
-        self.model = "BAAI/bge-small-en-v1.5"
+        self.model = SentenceTransformer("BAAI/bge-small-en-v1.5")
 
     def embed(self, chunks):
-        vectors = [self.client.feature_extraction(text, model=self.model) for text in chunks]
+        vectors = self.model.encode(chunks)
         return [np.array(v).flatten().tolist() for v in vectors]
 
     def embed_q(self, query):
-        v = self.client.feature_extraction(query, model=self.model)
+        v = self.model.encode(query)
         return np.array(v).flatten().tolist()
