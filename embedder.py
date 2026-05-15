fddbe83c4013ffa0bfce7ffc6cac1451cc7ad277
Aarav <gamingtiger2.0.12072010@gmail.com>
2026-05-15 11:58:13 +0530

    Refactor Embedder to use InferenceClient for embedding

--- embedder.py
+++ embedder.py
@@ -1,15 +1,16 @@
 import os
 import numpy as np
-from sentence_transformers import SentenceTransformer
+from huggingface_hub import InferenceClient
 
 class Embedder:
     def __init__(self):
-        self.model = SentenceTransformer("BAAI/bge-small-en-v1.5")
+        self.client = InferenceClient(token=os.environ["HF_TOKEN"])
+        self.model = "BAAI/bge-small-en-v1.5"
 
     def embed(self, chunks):
-        vectors = self.model.encode(chunks,batch_size = 32)
+        vectors = [self.client.feature_extraction(chunks, model=self.model,batch_size = 32)]
         return [np.array(v).flatten().tolist() for v in vectors]
 
     def embed_q(self, query):
-        v = self.model.encode(query)
+        v = self.client.feature_extraction(query, model=self.model)
         return np.array(v).flatten().tolist()
