8bb66e78e2e2bffc7368f9eb9c02733d2f4e9558
Aarav <gamingtiger2.0.12072010@gmail.com>
2026-06-13 11:12:18 +0530

    added fitz and removed pymupdf4llm

--- loader.py
+++ loader.py
@@ -9,7 +9,21 @@ class Loader:
         self.file = file_path
 
     def load(self):
-        pymupdf4llm.use_layout(False)
-        text = pymupdf4llm.to_markdown(self.file)
+        text_chunks = []
+    
+    
+        doc = fitz.open(pdf_path)
+    
+        for page in doc:
+        
+            page_text = page.get_text("text")
+        
+        
+            page_text = "\n".join([line.strip() for line in page_text.split("\n") if line.strip()])
+        
+            text_chunks.append(page_text)
+        
+        doc.close()
+        return "\n\n".join(text_chunks)
 
         return text
