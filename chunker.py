06cc8409e48286526a1153b30048da46f8386e7e
Aarav <gamingtiger2.0.12072010@gmail.com>
2026-06-13 11:16:28 +0530

    made the unneccesary complex chunking simpler

--- chunker.py
+++ chunker.py
@@ -1,31 +1,34 @@
-import re
 class Chunker:
     def __init__(self, chunk_size=250, overlap=40):
         self.chunk_size = chunk_size
         self.overlap = overlap
-        self.patterns =  r"\|.+\|.*?\n\| *[-:]+ *\|.*?\n(?:\|.+\|.*?\n?)*"
 
     def chunker(self, text):
         if self.overlap >= self.chunk_size:
             raise ValueError("Overlap must be smaller than chunk size.")
-        
-        clean_text = text
 
         chunks = []
-        tables = []
         start = 0
-        #text_size = len(text)
-        matches = re.finditer(self.patterns,text)
-        table_intervals = [(m.start(),m.end()) for m in matches]
-        table_intervals.sort(reverse=True)
+        text_size = len(text)
 
-        for start_indx , end_indx in table_intervals:
-            tab = text[start_indx:end_indx]
-            if tab:
-                clean_text = clean_text[:start_indx] + clean_text[end_indx:]
-                tables.append(tab)
-        text_size = len(clean_text)
+        while start < text_size:
+            end = start + self.chunk_size
+            
+            
+            if end < text_size:
+                last_space = text.rfind(' ', start, end)
+                if last_space != -1:
+                    end = last_space 
+            
 
+            chunk = text[start:end].strip()
+            if chunk:
+                chunks.append(chunk)
+            
+            
+            start = end - self.overlap
+            
+        return chunks
 
         while start < text_size:
             end = start + self.chunk_size
