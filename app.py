b4339efb7208b80128f10fa1eaa346acf09774f2
Aarav <gamingtiger2.0.12072010@gmail.com>
2026-06-16 14:15:57 +0530

    Refactor PDF upload and chat handling

--- app.py
+++ app.py
@@ -59,10 +59,9 @@ async def upload_pdf(file: UploadFile = File(...)):
         text     = Loader(tmp_path).load()
         chunks = Chunker().chunker(text)
         tokenized_chunks = [re.findall(r"\w+", chunk.lower())for chunk in chunks]
-        all_chunks = chunks + tables
-        vectors  = embedder.embed(all_chunks)
+        vectors  = embedder.embed(chunks)
         store    = VectorStorage(dimension=len(vectors[0]))
-        store.add(vectors, all_chunks)
+        store.add(vectors, chunks)
         bm25 = BM25Okapi(tokenized_chunks)
     finally:
         os.unlink(tmp_path)
